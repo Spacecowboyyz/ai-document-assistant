3691f4473bfd6d07ed0e18401e2b898930472b5e
Spacecowboyyz <krishsharma0161@gmail.com>
2026-05-23 15:09:49 +0530

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -73,7 +73,10 @@ def create_app() -> FastAPI:
     )
     application.add_middleware(
         CORSMiddleware,
-        allow_origins=settings.cors_origin_list,
+        allow_origins=["http://localhost:3000",
+        "https://ai-document-assistant-pearl.vercel.app",
+        "https://ai-document-assistant-ddjnjdzx3-spacecowboyyzs-projects.vercel.app",
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
